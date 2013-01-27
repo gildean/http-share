2c269e80e3083da03abf7bac29f85b48b3831fe7
gildean <gildean@salaliitto.com>
2013-01-27 17:37:34 +0200

    Add subdir disabling

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-module.exports = function (port, path, auth, log) {
+module.exports = function (port, path, auth, log, nodirlists) {
     "use strict";
     var http = require('http'),
         connect = require('connect'),
@@ -6,6 +6,7 @@ module.exports = function (port, path, auth, log) {
         server = http.createServer(app).listen(port),
         usr;
 
+    // we need to wait for one tick before emitting events, so that the server is actually returned first
     if (auth) {
         usr = auth.split(':');
         app.use(connect.basicAuth(usr[0], usr[1]));
@@ -13,6 +14,7 @@ module.exports = function (port, path, auth, log) {
             server.emit('authed');
         });
     }
+    
     if (log) {
         app.use(connect.logger(log));
         process.nextTick(function () {
@@ -20,13 +22,20 @@ module.exports = function (port, path, auth, log) {
         });
     }
 
+    // static middleware handles reqs for favicons first, the favicon-middleware is a fallback
      app.use(connect.static(path))
-        .use(connect.favicon(__dirname + '/favicon.ico'))
-        .use(connect.directory(path))
-        .use(function (req, res) {
-            res.writeHead(418, {'Content-Type': 'text/html'});
-            res.end('<!DOCTYPE html>\n<html>\n  <head>\n    <meta charset="utf-8">\n    <title>http-share</title>\n  </head>\n    <body>\n    <h1 style="font-size:1100%;color:#696969;position:fixed;bottom:0;left:0;padding:0;margin:0">☕<i style="font:italic 42% Georgia, Times, serif;">On a coffeebreak...</i></h1>\n  </body>\n</html>\n');
-        });
+        .use(connect.favicon(__dirname + '/favicon.ico'));
+    
+    // the directorylistings are created unless explicitly disabled
+    if (!nodirlists) {
+        app.use(connect.directory(path));
+    }
+    
+    // a fallback response for everything that falls through
+    app.use(function (req, res) {
+        res.writeHead(418, {'Content-Type': 'text/html'});
+        res.end('<!DOCTYPE html>\n<html>\n  <head>\n    <meta charset="utf-8">\n    <title>http-share</title>\n  </head>\n    <body>\n    <h1 style="font-size:1100%;color:#696969;position:fixed;bottom:0;left:0;padding:0;margin:0">☕<i style="font:italic 42% Georgia, Times, serif;">On a coffeebreak...</i></h1>\n  </body>\n</html>\n');
+    });
 
     return server;
 };
